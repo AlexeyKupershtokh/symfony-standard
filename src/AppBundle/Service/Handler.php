70fa6a00dc410e5ce0fffc76da13fe52ce6755d9
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2015-03-27 09:14:22 +0600

    Updated thrift

--- src/AppBundle/Service/Handler.php
+++ src/AppBundle/Service/Handler.php
@@ -7,6 +7,32 @@ use Overblog\ThriftBundle\Api\Extensions\BaseExtension;
 
 class Handler extends BaseExtension implements ServiceIf
 {
+    /**
+     */
+    public function ping()
+    {
+        // TODO: Implement ping() method.
+    }
+
+    /**
+     * @param int $id
+     * @return \AppBundle\ThriftModel\Test[]
+     * @throws \AppBundle\ThriftModel\InvalidValueException
+     */
+    public function getList($id)
+    {
+        // TODO: Implement getList() method.
+    }
+
+    /**
+     * @param \AppBundle\ThriftModel\Test $test
+     * @return int
+     * @throws \AppBundle\ThriftModel\InvalidValueException
+     */
+    public function create(\AppBundle\ThriftModel\Test $test)
+    {
+        // TODO: Implement create() method.
+    }
 
     public function ping()
     {
